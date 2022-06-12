e167f188d524ad1df31702b84b1f756b88d81478
StanislavVolk <stanislav.almaty@gmail.com>
2022-06-12 15:20:17 +0600

    Initial commit

---
+++ src/test/java/com/geekbrains/App2Test.java
@@ -0,0 +1,26 @@
+package com.geekbrains;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class App2Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        System.out.println("Не удалось установить sure_fire, как на лекции...");
+    }
+}
---
+++ src/test/java/com/geekbrains/AppTest.java
@@ -0,0 +1,26 @@
+package com.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        System.out.println("Но почему - то все получилось...");
+    }
+}
