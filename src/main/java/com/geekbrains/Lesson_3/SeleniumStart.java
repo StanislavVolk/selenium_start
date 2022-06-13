b5b6784cf38cccafac7ceea04321323373d6f918
StanislavVolk <stanislav.almaty@gmail.com>
2022-06-13 14:20:20 +0600

    Initial commit

---
+++ src/main/java/com/geekbrains/Lesson_3/SeleniumStart.java
@@ -0,0 +1,24 @@
+package com.geekbrains.Lesson_3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumStart
+{
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver ffDriver = new FirefoxDriver();
+        ffDriver.get("https://ya.ru");
+        Thread.sleep(5000);
+        ffDriver.quit();
+        driver.quit();
+    }
+}
